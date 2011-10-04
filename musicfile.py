137ccfd03355e9ffec7ac7607ff7bd8af3209115
gcmalloc <malik.bougacha@epfl.ch>
2011-10-04 14:22:13 +0200

    checking style and correcting it, according to pep8

--- musicfile.py
+++ musicfile.py
@@ -5,104 +5,115 @@ from mutagen.flac import FLAC
 super class for all the music file of all type
 give the way to access in a simple fashion the tags
 """
+
+
 class MusicFile():
-   """
-   dictionnary to transform the standardised id3 tag to a more human 
-   readable tag, this dictionnary is also a list of tag we extract from 
-   the music file
-   """
-   USEFUL_TAG = {'TPE1':'artist', 'TALB':'album', 'TIT2':'title',
-                 'TRCK':'track', 'TCOM':'composer','TDAT':'date',
-                  'TYER':'year'}
+    """
+    Dictionnary to transform the standardised id3 tag to a more human
+    readable tag, this dictionnary is also a list of tag we extract from
+    the music file
+    """
+    USEFUL_TAG = {'TPE1': 'artist', 'TALB': 'album', 'TIT2': 'title',
+                  'TRCK': 'track', 'TCOM': 'composer', 'TDAT': 'date',
+                  'TYER': 'year'}
 
-   def __init__(self, path, open_function):
-      self.path = path
-      try:
-         self.audio = open_function(path)
-      except:
-         self = None
-         return
-      self.tags = dict()
-      self.parse_tag()
-      
-   """
-   init helper to extract the tags from the  
-   """
-   def parse_tag():
-      for k in self.USEFUL_TAG:
-         v = self.USEFUL_TAG[k]
-         #we consider the tag none existant if the value is empty
-         if v != "":
-            try:
-               self.tags[v] = self.audio[k]
-            except KeyError:
-               pass
-               
-   #sanitize method, clean the tag using the following technique
-   #just idea for the moment
-   
-   def sanitize_with_musicBrainz(self):
-      pass
+    """
+    initialisation function
+    @param path:
+    @param function used to get the tags
+    """
+    def __init__(self, path, open_function):
+        """
+        absolute path to the path
+        """
+        self.path = path
+        try:
+            self.audio = open_function(path)
+        except:
+            self = None
+            return
+        self.tags = dict()
+        self.parse_tag()
 
-   def guess_musicbrainz(self):
-      pass
-   
-   """
-   guess the title from the sound using 
-   """
-   def guess_sound(self):
-      pass
-   
-   #end of sanitizing methods
-   """
-   get the tag, the name of the tag must be in the value of USEFUL_TAG
-   @param the name of the tag
-   @return the value of the tag
-   """
-   def __getitem__(self, key):
-      return self.tags[key]
+    """
+    init helper to extract the tags from the
+    """
+    def parse_tag(self):
+        for k in self.USEFUL_TAG:
+            v = self.USEFUL_TAG[k]
+            #we consider the tag none existant if the value is empty
+            if v != "":
+                try:
+                    self.tags[v] = self.audio[k]
+                except KeyError:
+                    pass
 
-   """
-   set the tag, the name of the tag must be in the value of USEFUL_TAG
-   @param the key of the tag
-          the new value of the tag  
-   """
-   def __setitem__(self, key, value):
-      self.tags[key] = str(value)
+    #sanitize method, clean the tag using the following technique
+    #just idea for the moment
 
-   def keys(self):
-         return self.tags.keys()
+    def sanitize_with_musicBrainz(self):
+        pass
 
-   """
-   test if the music file contain a key
-   """
-   def has_key(self, key):
-      return self.tags.has_key(key)
-   
-   """
-    print all tags that mutagen can find
-    @param maximum length of all the content
-   """
-   def __str__(self, maxsize=100):
-      for k in self.audio.keys():
-         if len(str(self.audio[k])) < maxsize:
-            sys.stdout.write("   " + k + ":")
-            print self.audio[k]=
+    def guess_musicbrainz(self):
+        pass
 
-   """
-    @return the path if the condition for the existence of the tags are fullfilled
+    """
+    guess the title from the sound using a sound
+    """
+    def guess_sound(self):
+        pass
+
+    #end of sanitizing methods
+    """
+    get the tag, the name of the tag must be in the value of USEFUL_TAG
+    @param the name of the tag
+    @return the value of the tag
+    """
+    def __getitem__(self, key):
+        return self.tags[key]
+
+    """
+    set the tag, the name of the tag must be in the value of USEFUL_TAG
+    @param the key of the tag
+    the new value of the tag
+    """
+    def __setitem__(self, key, value):
+        self.tags[key] = str(value)
+        #write down the tags
+
+    def keys(self):
+            return self.tags.keys()
+
+    """
+    test if the music file contain a key
+    """
+    def has_key(self, key):
+        return key in self.tags
+
+    """
+     print all tags that mutagen can find
+     @param maximum length of all the content
+    """
+    def __str__(self, maxsize=100):
+        for k in self.audio.keys():
+            if len(str(self.audio[k])) < maxsize:
+                sys.stdout.write("  " + k + ":")
+                print self.audio[k]
+
+    """
+    @return the path if the condition for the existence of the tags
+            are fullfilled
             None otherwise
-   """
-   def move_with_condition(self):
-      if condition_tester():
-            return path.substitue#syntax
-      else:
-         return none
-   
-   """
-   test if the file match the matching params on the tags
-   a way to test the presence of tags in the method
-   """
-   def condition_tester(self, matching):
-      pass
-   
+    """
+    def move_with_condition(self):
+        if condition_tester():
+                return path.substitue  # syntax
+        else:
+            return none
+
+    """
+    test if the file match the matching params on the tags
+    a way to test the presence of tags in the method
+    """
+    def condition_tester(self, matching):
+        pass
--- musicsorter.py
+++ musicsorter.py
@@ -11,144 +11,164 @@ import musicfile
 """
 Directory walker, handle the creation of the musicfile classes instances
 """
+
+
 class MusicWalker(threading.Thread):
-   
-   """
-   Integer, correspond to the number of file already read
-   """
-   music_file_count = 0
-   
-   """
-   Initialisation of the magic file library
-   """
-   magic = magic.Magic()
-   
-   """
-   dictionnary that establish a correspondance between mime type and 
-   music type file, as handled by the musicfile module
-   """
-   MUSIC_TYPES = {'audio/mpeg':'mp3','audio/flac':'flac'}
-   
-   """
-   Default initialisation 
-   """
-   def __init__(self, path, args):
-      threading.Thread.__init__(self)
-      self.path = path
-      self.args = args
-    
-   """
-   launch the walker, create MusicFile instance accoring to their type
-   then handle 
-   """
-   def run(self):
-      dir_ls = os.listdir(self.path)
-      for element in dir_ls:
-         if os.path.isdir(element):
-            #recursive call
-            music_dir = MusicWalker(i)
-            music_dir.run()
-         else:
-            file_type = get_music_type()
-            if file_type:
-               music_file = MusicFile(os.path.join(self.path, i))
-               #sanitize the file's tag according to the parameters stored 
-               #in flag
-               if music_file:
-                  if args.flag_print:
-                     print music_file
-                  if args.flag_capital:
-                     music_file.capitalize_all()
-                  if args.flag_move():
-                     music_file.move_with_condition()
-                  if arg.flag_brainz:
-                     music_file.sanitize_with_musicBrainz()
-                  if arg.flag_brainz_force:
-                     music_file.guess_musicbrainz()
-                  if arg.flag_audio_guess:
-                     music_file.guess_sound()
-                  if args.flag_move:
-                     fil.move_with_condition(args)
-                     
-   """
-   extract the type of a file according to the music file notation
-   @param the absolute path of the music file
-   @return  None if the file is not a recognised music file
-            the music file type according to the music type notation
-   """
-   @staticmethod
-   def get_music_type(music_file):     
-      music_type = get_type(music_file)
-      if music_type:
-         return music_type
-      else:
-         return None
-   
-   """
-   Wrapper around the magic module to handle audio file only.
-   @param the absolute path to the file we want to analyse
-   @return None if the mime_type s not in the MUSIC_TYPES dict.
-   """
-   @staticmethod
-   def get_type(music_file):
-      mime_type = self.magic.from_file(music_file, mime=True)
-      music_type = self.MUSIC_TYPES.get(mime_type)
-      return music_type
+
+    """
+    Integer, correspond to the number of file already read
+    """
+    music_file_count = 0
+
+    """
+    Initialisation of the magic file library
+    """
+    magic = magic.Magic()
+
+    """
+    dictionnary that establish a correspondance between mime type and
+    music type file, as handled by the musicfile module
+    """
+    MUSIC_TYPES = {'audio/mpeg': 'mp3', 'audio/flac': 'flac'}
+
+    """
+    Default initialisation
+    """
+    def __init__(self, path, args):
+        threading.Thread.__init__(self)
+        self.path = path
+        self.args = args
+
+    """
+    launch the walker, create MusicFile instance accoring to their type
+    then handle
+    """
+    def run(self):
+        dir_ls = os.listdir(self.path)
+        for element in dir_ls:
+            if os.path.isdir(element):
+                #recursive call
+                music_dir = MusicWalker(i)
+                music_dir.run()
+            else:
+                file_type = get_music_type()
+                if file_type:
+                    music_file = MusicFile(os.path.join(self.path, i))
+                    #sanitize the file's tag according to the parameters stored
+                    #in flag
+                    if music_file:
+                        if args.flag_print:
+                            print music_file
+                        if args.flag_capital:
+                            music_file.capitalize_all()
+                        if args.flag_move():
+                            music_file.move_with_condition()
+                        if arg.flag_brainz:
+                            music_file.sanitize_with_musicBrainz()
+                        if arg.flag_brainz_force:
+                            music_file.guess_musicbrainz()
+                        if arg.flag_audio_guess:
+                            music_file.guess_sound()
+                        if args.flag_move:
+                            fil.move_with_condition(args)
+
+    """
+    extract the type of a file according to the music file notation
+    @param the absolute path of the music file
+    @return  None if the file is not a recognised music file
+                the music file type according to the music type notation
+    """
+    @staticmethod
+    def get_music_type(music_file):
+        music_type = get_type(music_file)
+        if music_type:
+            return music_type
+        else:
+            return None
+
+    """
+    Wrapper around the magic module to handle audio file only.
+    @param the absolute path to the file we want to analyse
+    @return None if the mime_type s not in the MUSIC_TYPES dict.
+    """
+    @staticmethod
+    def get_type(music_file):
+        mime_type = self.magic.from_file(music_file, mime=True)
+        music_type = self.MUSIC_TYPES.get(mime_type)
+        return music_type
 
 """
 Flag handler, rename the flag, create the matching pattern for the music
 file tags
 """
+
+
 class Params():
-   
-   """
-   regex for the part we want to replace by a real call
-   """   
-   MATCH_REGEX = '{([^{}]*)}'
-   """
-   init function 
-   @param an instance of the ArgumentParser class
-   """
-   def __init__(self, args):
-      self.flag_count = args.c
-      self.flag_print = args.p
-      self.flag_capital = args.C
-      self.flag_brainz = args.b
-      self.flag_brainz_force = args.B
-      self.flag_audio_guess = args.a
-      #TODO
-      self.flag_move = False
-      self.match = self.replace_match(args.m[0])
-      
-   """
-   replace to match to a python expression
-   Not safe for now
-   @return the python expression
-   """
-   def replace_match(self, match):
-      while re.search(self.MATCH_REGEX, match):
-         match = re.sub(self.MATCH_REGEX, 'self.has_key(\'\\1\')', match)
-      return match
-   
+
+    """
+    regex for the part we want to replace by a real call
+    """
+    MATCH_REGEX = '{([^{}]*)}'
+
+    """
+    init function
+    @param an instance of the ArgumentParser class
+    """
+    def __init__(self, args):
+        self.flag_count = args.c
+        self.flag_print = args.p
+        self.flag_capital = args.C
+        self.flag_brainz = args.b
+        self.flag_brainz_force = args.B
+        self.flag_audio_guess = args.a
+        #TODO
+        self.flag_move = False
+        self.match = self.replace_match(args.m[0])
+
+    """
+    replace to match to a python expression
+    Not safe for now
+    @return the python expression
+    """
+    def replace_match(self, match):
+        while re.search(self.MATCH_REGEX, match):
+            match = re.sub(self.MATCH_REGEX, 'self.has_key(\'\\1\')', match)
+        return match
+
 """
 main
 handle the parsing of the argument and the first call to MusicWalker
 """
+
+
 def main():
-   parser = argparse.ArgumentParser(description='Sort Music according to id3 tags')
-   parser.add_argument('-c', action="store_true", default=False, help="Count the number of Music files that match the descriptor")
-   parser.add_argument('-p', action="store_true", default=False, help="Print the path of all the file matching the paramerers")
-   parser.add_argument('-C', action="store_true", default=False, help="Capitalize each word in all the tags")
-   parser.add_argument('-b', action="store_true", default=False, help="Use music Brainz database to correct existing tags")
-   parser.add_argument('-B', action="store_true", default=False, help="Use music Brainz database to try to guess non existing tags")
-   parser.add_argument('-a', action="store_true", default=False, help="Use the audio print of file to guess the uncomplete tag")
-   parser.add_argument('-m', action="store", metavar="conditions", type=str, nargs=1, help="File descriptor, ban be list of matching argument separeted by & and |, for respectively and and or. the arguments are in the list of argument")
-   #parser.add_argument('-s', metavar="location", help="Move the music file according to the list argument, each argument which will be replaced is of this form :{element}")
-   parser.add_argument('directories', nargs='+', action="store", metavar="dir", type=str, help="The location of the directories")
-   args = parser.parse_args(["-m", "{title}&{album}", "/home/malik/Public"])
-   print args
-   Params(args)
-   dir = "/home/malik/Public"
-   MusicDir(dir, args).start()
+    parser = argparse.ArgumentParser(description='Sort Music according \
+    to id3 tags')
+    parser.add_argument('-c', action="store_true", default=False, \
+    help="Count the number of Music files that match the descriptor")
+    parser.add_argument('-p', action="store_true", default=False, \
+    help="Print the path of all the file matching the paramerers")
+    parser.add_argument('-C', action="store_true", default=False, \
+    help="Capitalize each word in all the tags")
+    parser.add_argument('-b', action="store_true", default=False, \
+    help="Use music Brainz database to correct existing tags")
+    parser.add_argument('-B', action="store_true", default=False, \
+    help="Use music Brainz database to try to guess non existing tags")
+    parser.add_argument('-a', action="store_true", default=False, \
+    help="Use the audio print of file to guess the uncomplete tag")
+    parser.add_argument('-m', action="store", metavar="conditions", \
+    type=str, nargs=1, help="File descriptor, ban be list of matching \
+    argument separeted by & and |, for respectively and and or. the \
+    arguments are in the list of argument")
+    #parser.add_argument('-s', metavar="location", \
+    #help="Move the music file according to the list argument, each \
+    #argument which will be replaced is of this form :{element}")
+    parser.add_argument('directories', nargs='+', action="store", \
+    metavar="dir", type=str, help="The location of the directories")
+    args = parser.parse_args(["-m", "{title}&{album}", "/home/malik/Public"])
+    print args
+    Params(args)
+    dir = "/home/malik/Public"
+    MusicDir(dir, args).start()
 
 main()
