b93c7932c3d5b9a1e0b2242e352a474380752917
gcmalloc <gcmalloc@gmail.com>
2012-04-06 21:33:27 +0200

    adding filecluster for path information detection

---
+++ src/musicfilecluster.py
@@ -0,0 +1,11 @@
+
+class MusicFileCluster():
+    
+    def __init__(self, musicFile*):
+        self.musicFiles = set()
+        for i in musicFile:
+            self.musicFiles.add()
+
+    def weight_files():
+        #files are together so they should look a bit the same
+        
